6a69b64c3f875a916eaf3af6b0477fecf5c27a39
Tadeujr <tadeu_junior@outlook.com>
2019-08-18 12:27:20 -0300

    configurando a dev

--- main.ts
+++ main.ts
@@ -3,3 +3,4 @@ import {Medicamento} from "./Medicamento"
 let newUser = new Medicamento("Doril","Dipirona Sódica");
 
 console.log(newUser.getPrincipioAtivo() +"\n" + newUser.getnomeMedicamento());
+console.log("Passou?")
