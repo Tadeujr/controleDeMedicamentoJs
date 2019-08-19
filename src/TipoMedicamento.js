0f048fcdc203bac16b950b30d185807561aa82c8
Tadeujr <tadeu_junior@outlook.com>
2019-08-19 14:16:52 -0300

    teste

---
+++ src/TipoMedicamento.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var TipoMedicamento = /** @class */ (function () {
+    function TipoMedicamento(medicamento, qtdRetirada) {
+        this.medicamento = medicamento;
+        this.qtdRetirada = qtdRetirada;
+    }
+    return TipoMedicamento;
+}());
