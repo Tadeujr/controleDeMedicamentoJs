c104c830a2c8dfe59773bcfa9da30c78f49e5630
Tadeu da Penha Moraes Junior <tadeu_junior@outlook.com>
2019-09-20 13:50:28 -0300

    Update usuarioE.entity.ts
    anotaçoes sobre o DAO

--- src/entidades/usuarioE.entity.ts
+++ src/entidades/usuarioE.entity.ts
@@ -1,5 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
-
+// tem que modificr essa classe e fazendo ela herdar os metodos da interface DAO que ainda não criei
 @Entity()
 export class UsuarioE {
   @PrimaryGeneratedColumn()
